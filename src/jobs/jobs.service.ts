2b3d7e34229156bf7493e75c8906d6af3a118daa
jokerf6 <fhakem75@gmail.com>
2023-04-03 06:52:22 +0200

    erase save

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -85,8 +85,7 @@ export class JobsService {
           code: query.jobLocation ? query.jobLocation : userTitle.cityId,
         },
       },
-      skip: parseInt(query.skip) || 0,
-      take: +query.take || 6,
+
       orderBy: { createdAt: "desc" },
       include: {
         company: true,
@@ -203,8 +202,7 @@ export class JobsService {
           },
         },
       },
-      skip: parseInt(query.skip) || 0,
-      take: +query.take || 6,
+
       select: {
         jobs: true,
       }, ////
